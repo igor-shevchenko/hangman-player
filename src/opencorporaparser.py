89625a0f0553a9c66edfb51855fac5edaa8f40b5
Igor Shevchenko <phar0s@ya.ru>
2012-08-21 23:08:34 +0600

    Open Corpora Parser

---
+++ src/opencorporaparser.py
@@ -0,0 +1,30 @@
+
+
+class OpenCorporaParser:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_words(self, requirements):
+        return (word for word in (self.get_words(self.extract_lines(requirements))))
+
+    def extract_lines(self, requirements):
+       return (line for line in open(self.filename) if requirements.are_satisfied_by(line))
+
+    def get_words(self, lines):
+        return (line.split('\t')[0] for line in lines)
+
+
+
+class OpenCorporaParserExtractionRequirements:
+    def __init__(self, required_tags, restricted_tags):
+        self.required_tags = required_tags
+        self.restricted_tags = restricted_tags
+
+    def are_satisfied_by(self, line):
+        for tag in self.required_tags:
+            if line.search(tag) == -1:
+                return False
+        for tag in self.restricted_tags:
+            if line.search(tag) != -1:
+                return False
+        return True
