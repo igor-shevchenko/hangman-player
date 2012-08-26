8b5e3f6c71ce7d439ab8af08106a04e729b4e35e
Igor Shevchenko <phar0s@ya.ru>
2012-08-26 23:03:54 +0600

    guesser

--- src/hangman/guesser.py
+++ src/hangman/guesser.py
@@ -5,17 +5,38 @@ class HangmanGuesser:
     def __init__(self, word_provider):
         self.word_provider = word_provider
 
-    def guess_letter(self, mask, guessed_letters, wrong_letters):
-        regex = mask.make_regex_for_mask(mask, wrong_letters, guessed_letters)
+    def guess_letter(self, word_mask, guessed_letters, wrong_letters):
+        disallowed_letters = wrong_letters + guessed_letters
+        regex = mask.make_regex_for_mask(word_mask, disallowed_letters)
         possible_words = self.get_possible_words(regex)
+        possible_letters = self.get_letter_statistics(possible_words)
+        for letter in possible_letters:
+            if letter not in disallowed_letters:
+                return letter
+        # if word not in dictionary
+
+        if word_mask != mask.UNGUESSED_LETTER_PLACEHOLDER * len(word_mask):
+            # try common statistics for words of that length
+            return self.guess_letter(mask.UNGUESSED_LETTER_PLACEHOLDE\
+                                     * len(word_mask), guessed_letters,
+                                     wrong_letters)
+        elif word_mask != mask.ALL_WORDS_MASK:
+            # try common statistics for all words
+            return self.guess_letter(mask.ALL_WORDS_MASK, guessed_letters,
+                                     wrong_letters)
+        else:
+            raise Exception()
 
     def get_possible_words(self, regex):
         return (word for word in self.word_provider.get_words() if regex.match(word))
 
-#    def get_letter_statistics(self, words):
-#        statistics = {}
-#        for word in words:
-#            for letter in set(word):
-#                statistics[letter] = statistics.get()
+    def get_letter_statistics(self, words):
+        statistics = {}
+        for word in words:
+            for letter in set(word):
+                statistics[letter] = statistics.get(letter, 0) + 1
+        result = statistics.items()
+        result.sort(key=lambda x: (x[1], x[0]), reverse=True)
+        return result
 
 
--- src/hangman/mask.py
+++ src/hangman/mask.py
@@ -3,6 +3,7 @@ import re
 
 UNGUESSED_LETTER_PLACEHOLDER = '*'
 ALL_LETTERS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+ALL_WORDS_MASK = '.*'
 
 def get_mask(word, guessed_letters):
     mask = [letter if letter in guessed_letters
@@ -10,12 +11,14 @@ def get_mask(word, guessed_letters):
             for letter in word]
     return ''.join(mask)
 
-def make_regex_for_mask(mask, wrong_letters, guessed_letters):
-    excluded_letters = wrong_letters + guessed_letters
-    allowed_letters = [letter for letter in ALL_LETTERS if letter not in excluded_letters]
+def make_regex_for_mask(mask, disallowed_letters):
+    allowed_letters = [letter for letter in ALL_LETTERS if letter not in disallowed_letters]
     allowed_letters = ''.join(allowed_letters)
     # it can be more efficient if a sequence of * was replaced with
     # "[letter_pattern]{n}"
     unguessed_letter_pattern = "[%s]" % allowed_letters
-    regex = '^%s$' % mask.replace(UNGUESSED_LETTER_PLACEHOLDER, unguessed_letter_pattern)
+    if mask == ALL_WORDS_MASK:
+        regex = '^%s+$' % unguessed_letter_pattern
+    else:
+        regex = '^%s$' % mask.replace(UNGUESSED_LETTER_PLACEHOLDER, unguessed_letter_pattern)
     return re.compile(regex, re.MULTILINE)
