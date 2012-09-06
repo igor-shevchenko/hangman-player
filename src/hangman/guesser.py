86373b3a6ed53970beebd0b4a78eecfd266fdfae
Igor Shevchenko <phar0s@ya.ru>
2012-09-06 22:34:39 +0600

    refactoring

--- src/hangman/guesser.py
+++ src/hangman/guesser.py
@@ -6,29 +6,25 @@ class HangmanGuesser:
         self.word_provider = word_provider
 
     def guess_letter(self, word_mask, guessed_letters, wrong_letters):
-        disallowed_letters = wrong_letters + guessed_letters
-        regex = mask.make_regex_for_mask(word_mask, disallowed_letters)
-        possible_words = self.get_possible_words(regex)
+        raise NotImplementedError()
+
+
+class DeterminedHangmanGuesser(HangmanGuesser):
+    def guess_letter(self, word_mask, guessed_letters, wrong_letters):
+        used_letters = wrong_letters + guessed_letters
+        regex = mask.make_regex_for_mask(word_mask, used_letters)
+        possible_words = self.word_provider.get_matching_words(regex)
         possible_letters = self.get_letter_statistics(possible_words)
         for letter in possible_letters:
-            if letter not in disallowed_letters:
+            if letter not in used_letters:
                 return letter
-        # if word not in dictionary
 
-        if word_mask != mask.UNGUESSED_LETTER_PLACEHOLDER * len(word_mask):
-            # try common statistics for words of that length
-            return self.guess_letter(mask.UNGUESSED_LETTER_PLACEHOLDER\
-                                     * len(word_mask), guessed_letters,
-                                     wrong_letters)
-        elif word_mask != mask.ALL_WORDS_MASK:
-            # try common statistics for all words
+        # if word not in dictionary, try common statistics for all words
+        if word_mask != mask.ALL_WORDS_MASK:
             return self.guess_letter(mask.ALL_WORDS_MASK, guessed_letters,
                                      wrong_letters)
-        else:
-            raise Exception()
 
-    def get_possible_words(self, regex):
-        return (word for word in self.word_provider.get_words() if regex.match(word))
+        raise Exception()
 
     def get_letter_statistics(self, words):
         statistics = {}
--- src/hangman/word_provider.py
+++ src/hangman/word_provider.py
@@ -1,11 +1,18 @@
-
-
 class WordProvider:
-    def __init__(self, filename):
-        f = open(filename)
-        words = f.readlines()
-        map(lambda word: word.strip(), words)
+    def __init__(self, words):
         self.words = words
 
     def get_words(self):
         return self.words
+
+    def get_matching_words(self, regex):
+        return (word for word in self.get_words() if regex.match(word))
+
+
+class FileWordProvider(WordProvider):
+    def __init__(self, filename):
+        f = open(filename)
+        words = f.readlines()
+        f.close()
+        map(lambda word: word.strip(), words)
+        WordProvider.__init__(self, words)
