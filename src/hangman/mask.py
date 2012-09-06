08602a7a848249c520e76eff10a8623e1a399196
Igor Shevchenko <phar0s@ya.ru>
2012-09-06 23:22:59 +0600

    efficiency improvement!

--- src/hangman/mask.py
+++ src/hangman/mask.py
@@ -14,11 +14,21 @@ def get_mask(word, guessed_letters):
 def make_regex_for_mask(mask, disallowed_letters):
     allowed_letters = [letter for letter in ALL_LETTERS if letter not in disallowed_letters]
     allowed_letters = ''.join(allowed_letters)
-    # it can be more efficient if a sequence of * was replaced with
-    # "[letter_pattern]{n}"
     unguessed_letter_pattern = "[%s]" % allowed_letters
     if mask == ALL_WORDS_MASK:
         regex = '^%s+$' % unguessed_letter_pattern
     else:
-        regex = '^%s$' % mask.replace(UNGUESSED_LETTER_PLACEHOLDER, unguessed_letter_pattern)
+        placeholder_count = 0
+        regex = '^'
+        for char in mask:
+            if char == UNGUESSED_LETTER_PLACEHOLDER:
+                placeholder_count += 1
+            else:
+                if placeholder_count > 0:
+                    regex = '%s%s{%d}' % (regex, unguessed_letter_pattern, placeholder_count)
+                    placeholder_count = 0
+                regex += char
+        if placeholder_count > 0:
+            regex = '%s%s{%d}' % (regex, unguessed_letter_pattern, placeholder_count)
+        regex += '$'
     return re.compile(regex, re.MULTILINE)
